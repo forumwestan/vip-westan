ff68cba8c54c5cf813de23d4b066272f8d40771d
forumwestan <contato@forumwestan.com>
2025-12-10 17:09:57 -0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,43 +1,34 @@
 # name: discourse-vip-color
-# about: Adiciona a cor personalizada (User Field) como classe CSS nos posts
-# version: 0.1
-# authors: SeuNome
+# about: Adiciona classe CSS de cor baseada em User Field para VIPs
+# version: 1.1
+# authors: Gemini
 # url: https://github.com/SEU-USUARIO/discourse-vip-color
+# required_version: 2.7.0
 
 enabled_site_setting :vip_color_enabled
 
 after_initialize do
-  # Pega o ID do campo definido nas configurações (padrão 6)
-  def get_color_field_id
-    SiteSetting.vip_color_field_id
-  end
-
-  # Lógica para extrair a cor
+  # Função segura para extrair a cor
+  # O Discourse armazena user_fields como um Hash onde as chaves são Strings
   get_vip_color = Proc.new do |user|
-    if user&.user_fields
-      field_id = get_color_field_id.to_s
+    if user && user.user_fields
+      field_id = SiteSetting.vip_color_field_id.to_s
       color = user.user_fields[field_id]
-      # Retorna a cor apenas se existir e não for vazia
+      # Retorna a cor apenas se não for nula e não estiver em branco
       color.present? ? color : nil
     else
       nil
     end
   end
 
-  # 1. Envia a cor junto com os dados do POST
+  # Adiciona a cor ao serializador do POST (para aparecer nos tópicos)
   add_to_serializer(:post, :user_vip_color) do
-    # Tenta pegar do objeto user carregado no post
-    user = object.user
-    get_vip_color.call(user) if user
+    # object.user pode ser nil se o usuário foi deletado
+    get_vip_color.call(object.user)
   end
 
-  # 2. Envia a cor junto com o User Card (clique no avatar)
+  # Adiciona a cor ao serializador do UserCard (clique no avatar)
   add_to_serializer(:user_card, :user_vip_color) do
-    get_vip_color.call(object)
-  end
-  
-  # 3. Envia a cor no perfil do usuário
-  add_to_serializer(:user, :user_vip_color) do
     get_vip_color.call(object)
   end
 end
