4c421290a70f2f4752d5f6041e270d38ed810b1b
forumwestan <contato@forumwestan.com>
2025-12-11 21:03:25 -0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,37 +1,52 @@
 # name: vip-westan
 # about: Adiciona cor ao nick apenas se o usuario for do grupo VIP e tiver o campo preenchido
-# version: 2.1
+# version: 2.2
 # authors: Westan
+# url: https://github.com/SEU-USUARIO/vip-westan
 
 enabled_site_setting :vip_color_enabled
 
 after_initialize do
-  # ID do campo de usuario (Confirme se é 6 no seu painel)
-  def get_field_id
-    SiteSetting.vip_color_field_id.to_s
-  end
-
-  # Nome exato do grupo (slug) - Geralmente é minusculo
-  def get_vip_group_name
-    "vip" 
-  end
-
+  # Lógica principal encapsulada em uma Proc para reutilização
   get_vip_data = Proc.new do |user|
+    # 1. Se o usuário não existir (ex: deletado), para aqui
     next nil unless user
 
-    # VERIFICAÇÃO DE SEGURANÇA:
-    # O usuário PRECISA estar no grupo 'vip' para a cor funcionar.
-    is_vip = user.groups.any? { |g| g.name == get_vip_group_name }
-    
+    # 2. Definições (Pega das configurações a cada chamada para garantir atualização)
+    field_id = SiteSetting.vip_color_field_id.to_s
+    vip_group_name = "vip"
+
+    # 3. Verifica se o usuário pertence ao grupo VIP
+    # Usamos .include? que é ligeiramente mais rápido que .any? para nomes de grupo
+    is_vip = user.groups.any? { |g| g.name == vip_group_name }
+
+    # 4. Se for VIP e tiver campos personalizados
     if is_vip && user.user_fields
-      color = user.user_fields[get_field_id]
-      # Retorna a cor em minusculo (ex: "Red" vira "red")
-      color.present? ? color.downcase : nil
+      color = user.user_fields[field_id]
+      
+      # Retorna a cor em minúsculo e sem espaços extras
+      if color.present?
+        color.to_s.strip.downcase
+      else
+        nil
+      end
     else
       nil
     end
   end
 
-  add_to_serializer(:post, :user_vip_color) { get_vip_data.call(object.user) }
-  add_to_serializer(:user_card, :user_vip_color) { get_vip_data.call(object) }
+  # Adiciona ao Serializer do Post (Para aparecer nos tópicos)
+  add_to_serializer(:post, :user_vip_color) do
+    get_vip_data.call(object.user)
+  end
+
+  # Adiciona ao Serializer do UserCard (Para aparecer ao clicar no avatar)
+  add_to_serializer(:user_card, :user_vip_color) do
+    get_vip_data.call(object)
+  end
+  
+  # Adiciona ao Serializer do Perfil (Para aparecer na página de perfil)
+  add_to_serializer(:user, :user_vip_color) do
+    get_vip_data.call(object)
+  end
 end
